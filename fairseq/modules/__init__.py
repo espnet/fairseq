fe87741a027fa8fc90990ab0dd833b2dac930761
Fhrozen <nyalta21@gmail.com>
2024-07-09 22:02:48 +0900

    adding files

---
+++ fairseq/modules/__init__.py
@@ -0,0 +1,8 @@
+from .layer_norm import Fp32LayerNorm, LayerNorm
+from .multihead_attention import MultiheadAttention 
+
+__all__ = [
+    "Fp32LayerNorm",
+    "LayerNorm",
+    "MultiheadAttention",
+]
